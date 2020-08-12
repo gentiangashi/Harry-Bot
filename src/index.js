11265510d44603f441c748a17bb1497496e61523
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-12 20:31:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const discord = require('discord.js');
 var client = new discord.Client();
 const token = "NzQzMTIxNTc4Mzg4MjI2MTY5.XzQD5g.3lRqT22TreWfFN_mBYBwQNuLmZw";
-
+// command used to call bot in server
 const prefix = "h!";
+// discord api reads discord message
 client.on("message", (message) => {
   if(message.author.bot) return;
   // Chooses image at random
@@ -11,4 +12,5 @@ client.on("message", (message) => {
         message.channel.send ( {files: ["../memes/" + imageNumber + ".png"]} )
       }
 });
+// server connects to discord api using token
 client.login(token);
