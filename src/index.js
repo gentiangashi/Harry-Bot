04302ddcee6dcec18c3dfd38b710a38ef432e621
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-12 20:26:50 +0100

    Fixed random image selection

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
-const prefix = "!h";
+const discord = require('discord.js');
+var client = new discord.Client();
+const token = "NzQzMTIxNTc4Mzg4MjI2MTY5.XzQD5g.3lRqT22TreWfFN_mBYBwQNuLmZw";
 
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-bot.on('message', (message) => {
-
-    if(message.content == 'ping') {
-       message.channel.send('pong!');
-    }
+const prefix = "h!";
+client.on("message", (message) => {
+  if(message.author.bot) return;
+  // Chooses image at random
+  if (message.content.startsWith (prefix + "meme")) {
+    let imageNumber = Math.floor(Math.random()* 6) +1
+        message.channel.send ( {files: ["../memes/" + imageNumber + ".png"]} )
+      }
 });
-// Chooses image at random
-if (message.content.startsWith (prefix + "random image")) {
- let imageNumber = Math.floor(Math.random()* 6) +1
-     message.channel.send ( {files: ["./memes/" + imageNumber + ".png"]} )
-   }
-
-bot.on('message', (message) => {
-  if(message.content == '!h memes') {
-   var number = 3;
-      var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-    switch (random) {
-      case 1: message.channel.send ('./memes/4ckyeah.jpg'); break;
-      case 2: message.channel.send ('./memes/2deep 4 u.jpg'); break;
-      case 3: message.channel.send ('./memes/1365390360568.png'); break;
-     
-bot.login('NzQzMTIxNTc4Mzg4MjI2MTY5.XzQD5g.mSlN-k-HjJCWwMVDcZQNKQ-IkuI');
+client.login(token);
