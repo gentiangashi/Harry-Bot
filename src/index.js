7792bf3a1975aa47da8e89b699d80130d95f8880
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-13 19:09:43 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 const discord = require('discord.js');
 var client = new discord.Client();
-const token = "your-token-goes-here";
+require('dotenv-flow').config()
+Images = 13;
 
+// Config object
+const config = {
+  token: process.env.TOKEN,
+  owner: process.env.OWNER,
+  prefix: process.env.PREFIX
+};
+
+// Displays in command line when successfully connected
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// command used to call bot in server
-const prefix = "!h ";
-
-// discord api reads discord message
+// Discord API reads discord message
 client.on("message", (message) => {
   if(message.author.bot) return;
   
-  if (message.content.startsWith (prefix + "meme")) {
-    // Change 6 to however many images there are within the folder
-    let imageNumber = Math.floor(Math.random()* 13) +1
+  if (message.content.startsWith (config.prefix + "meme")) {
+    // Change 13 to however many images there are within the folder
+    let imageNumber = Math.floor(Math.random()* Images) +1
     // displays image at random
         message.channel.send ( {files: ["../memes/" + imageNumber + ".png"]} )
       }
 });
 // server connects to discord api using token
-client.login(token);
+client.login(config.token);
