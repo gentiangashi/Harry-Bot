2c4f2b26cab15a907fa4a40296714a1ab996dc69
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-13 18:38:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const discord = require('discord.js');
 var client = new discord.Client();
 const token = "your-token-goes-here";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 // command used to call bot in server
 const prefix = "!h ";
+
 // discord api reads discord message
 client.on("message", (message) => {
   if(message.author.bot) return;
+  
   if (message.content.startsWith (prefix + "meme")) {
     // Change 6 to however many images there are within the folder
     let imageNumber = Math.floor(Math.random()* 13) +1
