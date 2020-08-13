33b107c9ade201da9f1694f43c9c662ff17728a6
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-13 20:11:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 var client = new discord.Client();
 require('dotenv-flow').config()
-Images = 13; // Change number to however many images there are within the folder
+numberOfImages = 13; // Change number to however many images there are within the memes folder
 
 // Config object
 const config = {
@@ -20,7 +20,7 @@ client.on("message", (message) => {
   if(message.author.bot) return;
   
   if (message.content.startsWith (config.prefix + "meme")) {
-    let imageNumber = Math.floor(Math.random()* Images) +1
+    let imageNumber = Math.floor(Math.random()* numberOfImages) +1
     // displays image at random
         message.channel.send ( {files: ["../memes/" + imageNumber + ".png"]} )
       }
