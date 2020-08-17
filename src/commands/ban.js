af700be56c4f3cddc6a6312f14f1bd2190297e37
Gentian Gashi <genti_gashi8@hotmail.com>
2020-08-17 06:19:53 +0100

    Update ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -10,43 +10,49 @@ module.exports = {
 }
 
 module.exports.run = async(client, message, args) => {
-	if (!message.mentions.users.size) {
-            return message.reply('You need to tag a user in order to kick them!');
+        if (!message.mentions.users.size) {
+            return message.reply('You need to tag a user in order to ban them!');
         } else {
             let toBan = message.mentions.users.first();
-            let reason = args.join(' ').slice(22);
-            
-            if(!reason) return message.channel.send("No reason specified");
+            let reason = args.join(" ").slice(22);
+            let server = message.guild;
+
+            if (!toBan) {
+                message.channel.send('Can\'t find user!');
+            }
+            if(!toBan === message.author) return message.channel.send(`You Cant Ban Yourself`);
+            if(!reason) return message.channel.send(`${message.author} Please Provided A Reason`);
+         
             if (!message.member.hasPermission('MANAGE_MESSAGES')) {
-                return message.channel.send('Sorry You Dont Have Permission To Ban User');
+                return message.channel.send('Sorry You Dont Have Permission To Kick');
             }
             let inline = true;
-            let embed = new Discord.MessageEmbed()
+            let embed = new discord.MessageEmbed()
             .setTitle(`**Banned**`)
-            .setColor('PURPLE')
-            .setDescription(`**You Were Banned From ${message.guild.name}**`)
+            .setColor('RED')
+            .setDescription(`**You Were Banned From ${server.name}**`)
             .addField('Banned By', `${message.author.tag}`, inline)
             .addField('Reason:', reason)
             .setTimestamp()
             toBan.send(embed);
             let modlog = message.guild.channels.cache.find(ch => ch.name === 'mod-log');
     
-            let banEmbed = new Discord.MessageEmbed()
+            let banEmbed = new discord.MessageEmbed()
             .setTitle('**Ban**')
-            .setColor('PURPLE')
+            .setColor('RED')
             .addField('Kicked User', `${toBan} with ID: ${toBan.id}`)
             .addField('Kicked By', `<@${message.author.id}> with ID: ${message.author.id}`)
             .addField('Kicked From', `${message.channel}`)
             .addField('Reason', `${reason}`)
             .setTimestamp(new Date())
             .setFooter('RIP');
+
             if(!modlog) {
-                return message.channel.send('Can\'t find incidents channel');
+                return message.channel.send('Can\'t find mod-log channel');
             }
 
             message.guild.member(toBan).ban(reason);
             modlog.send(banEmbed);
-
         }
-        message.delete();    
+        message.delete();
 }
